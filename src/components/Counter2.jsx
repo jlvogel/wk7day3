ac696c5a59ea774f23e2ccc934688873258c1a39
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-12-10 08:03:27 -0500

    added a 2nd Counter component, src/components/Counter2.jsx .  Tested that it displays in browser use inspect tools to see the Console log as well as the Components in Recat dev Tools.

--- src/components/Counter2.jsx
+++ src/components/Counter2.jsx
@@ -6,5 +6,29 @@ import { useState } from "react"
 
 export default function Counter(props) {
   // Declare the state
+  const [counter, setCounter] = useState(0)
+  // second piece of state
+  const [evenCounter, setEvenCounter] = useState(0)
+
+  console.log("I'm just a random log")
+
+  // Function to add one to the state
+  const addOne = () => {
+    // if counter is even before the update, update evenCounter
+    if (counter %2 === 0) {
+      setEvenCounter(evenCounter + 1)
+    }
+    // sets counter to its current value + 1
+    setCounter(counter + 1)
+  }
+
+  // The h1 display the counter and button runs addOne function
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <h1>{evenCounter}</h1>
+      <button onClick={addOne}>Click Me to Add One</button>
+    </div>
+  )
 }
 
